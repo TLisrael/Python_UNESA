89f225026eba0fc3d7ba5b6095352481c040d2ca
Thainá Israel <thainaluizaisrael@gmail.com>
2022-11-04 14:10:12 -0300

    listas e dicionarios

---
+++ dicionarios.py
@@ -0,0 +1,11 @@
+# Exemplo de dicionário
+
+pessoas = {
+    1111: ['nome 01'],
+    2222: ['nome 02'],
+    3333: ['nome 03'],
+    4444: ['nome 04'],
+}
+print(f'Todos = {pessoas}')
+print(f'Primeira pessoa = {pessoas[1111]}')
+print(f'Quarta pessoa = {pessoas[4444]}')
